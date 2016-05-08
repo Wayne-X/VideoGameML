42f032061e7221c8b3ea95aa4ca518f965c5a4c1
nix-goat <waynexun2017@u.northwestern.edu>
2016-05-08 00:27:49 -0500

    stats look at right times now

--- items prices/makeCSV.js
+++ items prices/makeCSV.js
@@ -33,15 +33,15 @@ function main(inStr){
 		console.log("writing " + String(i) + "/" + String(obj.length - 1));
 		w_ID = String(obj[i].ge_id);
 		w_name = String(obj[i].label);
-		for (j=30; j<obj[i].data.length; j++){
+		for (j=60; j<obj[i].data.length; j++){
 			w_timestamp = String(obj[i].data[j][0]);
 			w_price_now = String(obj[i].data[j][1]);
-			w_price_one = String(obj[i].data[j-1][1]);
-			w_price_seven = String(obj[i].data[j-7][1]);
-			w_price_thirty = String(obj[i].data[j-30][1]);
-			w_thirty_min = String(getMin(obj[i].data.slice(j-30, j)));
-			w_thirty_max = String(getMax(obj[i].data.slice(j-30, j)));
-			w_thirty_average = String(getAvg(obj[i].data.slice(j-30, j)));
+			w_price_one = String(obj[i].data[j-1*2][1]);
+			w_price_seven = String(obj[i].data[j-7*2][1]);
+			w_price_thirty = String(obj[i].data[j-30*2][1]);
+			w_thirty_min = String(getMin(obj[i].data.slice(j-30*2, j)));
+			w_thirty_max = String(getMax(obj[i].data.slice(j-30*2, j)));
+			w_thirty_average = String(getAvg(obj[i].data.slice(j-30*2, j)));
 			// console.log(w_ID + ',' +  w_name + ',' +  w_timestamp + ',' +  w_price_now + ',' +  w_price_one + ',' +  w_price_seven + ',' +  w_price_thirty + ',' +  w_thirty_min + ',' +  w_thirty_max + ',' +  w_thirty_average + '\n');
 			wstream.write(w_ID + ',' +  w_name + ',' +  w_timestamp + ',' +  w_price_now + ',' +  w_price_one + ',' +  w_price_seven + ',' +  w_price_thirty + ',' +  w_thirty_min + ',' +  w_thirty_max + ',' +  w_thirty_average + '\n');
 		}
